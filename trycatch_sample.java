b70ae69e065c5eeed7756905f799f624dcc11c3b
HARIKRISHNAN R <128880349+kte22cs061@users.noreply.github.com>
2023-12-03 10:14:49 +0530

    Create trycatch_sample.java
    A sample question to show the working of try and catch

---
+++ trycatch_sample.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[ ] args) {
+    try {
+      int[] myNumbers = {1, 2, 3};
+      System.out.println(myNumbers[0]);
+    } catch (Exception e) {
+      System.out.println("Something went wrong.");
+    }
+  }
+}
